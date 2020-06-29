76a440ddd868379c809caaae371d21c8a4cba39e
|Smirnov| <krugvik@mail.ru>
2020-06-29 21:20:12 +0300

    task1

---
+++ academy/src/by/academy/lesson2/task1.java
@@ -0,0 +1,52 @@
+package by.academy.lesson2;
+
+import java.util.Scanner;
+
+public class task1 {
+public static void main (String []Args) {
+	Scanner sc = new Scanner (System.in);
+	String monthName = sc.nextLine();
+	switch (monthName) {
+	case "€нварь":
+		System.out.println ("1");
+		break;
+	case "февраль":
+		System.out.println ("2");
+		break;
+	case "март":
+		System.out.println ("3");
+		break;
+	case "апрель":
+		System.out.println ("4");
+		break;
+	case "май":
+		System.out.println ("5");
+		break;
+	case "июнь":
+		System.out.println ("6");
+		break;
+	case "июль":
+		System.out.println ("7");
+		break;
+	case "август":
+		System.out.println ("8");
+		break;
+	case "сент€брь":
+		System.out.println ("9");
+		break;
+	case "окт€брь":
+		System.out.println ("10");
+		break;
+	case "но€брь":
+		System.out.println ("11");
+		break;
+	case "декабрь":
+		System.out.println ("12");
+		break;
+	}
+		
+		
+		
+	
+}
+}
