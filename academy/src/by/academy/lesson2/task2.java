358ab6bbb849cf0e89f6eaf1947fd801f66b4a63
|Smirnov| <krugvik@mail.ru>
2020-06-29 21:32:08 +0300

    task2

---
+++ academy/src/by/academy/lesson2/task2.java
@@ -0,0 +1,10 @@
+package by.academy.lesson2;
+
+public class task2 {
+public static void main (String[]Args) {
+	int i=3;
+	int j=4;
+	int k=i*i+j*j;
+	System.out.println(i + "*" + i + "+" + j + "*" + j + "=" + k);
+}
+}
