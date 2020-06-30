7cf6e7e58b3ff5bd20649c1a487cbb5ff2121f18
olgabogatova <bogatova-1999@mail.ru>
2020-06-30 15:12:40 +0300

    task 3

---
+++ academy/src/by/academy/lesson2/Task3.java
@@ -0,0 +1,12 @@
+package by.academy.lesson2;
+
+public class Task3 {
+public static void main (String [] Args) {
+	int  a = 2021;
+	if (a%2 == 0) {
+	System.out.println ("число четное");
+} else { 
+	System.out.println ("Число  нечетное");
+}
+}
+}
