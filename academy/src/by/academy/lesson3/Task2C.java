9c0548fe48a585b0134b240b15c691a30ee619b2
|Smirnov| <krugvik@mail.ru>
2020-07-01 21:00:34 +0300

    njkl

---
+++ academy/src/by/academy/lesson3/Task2C.java
@@ -0,0 +1,12 @@
+package by.academy.lesson3;
+
+import java.util.Scanner;
+
+public class Task2C {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Ведите значение");
+		int a = sc.nextInt();
+
+	}
+}
