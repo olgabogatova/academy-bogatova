1b042dff7b7a179f9d60964138588f1b351dd950
olgabogatova <bogatova-1999@mail.ru>
2020-07-03 20:22:14 +0300

    task4

--- academy/src/by/academy/lesson3/Task4C.java
+++ academy/src/by/academy/lesson3/Task4C.java
@@ -4,6 +4,35 @@ import java.util.Scanner;
 public class Task4C {
 public static void main (String []args) {
 	Scanner sc = new Scanner(System.in);
-	System.out.println("Введите числа");
+	System.out.println("Введите количество чисел");	
+	int a = sc.nextInt(); 											// a - количество чисел
+	
+	System.out.println("Введите " +  a + " чисел");
+	
+	int o=0; 														// o - отрицательные числа	
+	int p=0; 														// p -  положительные числа
+	int n=0; 														// n - нули 
+	
+	
+	for (int i=0; i<a; i++) {
+	int b = sc.nextInt(); 											// b - введенные числа
+																	// программа запрашивает количество чисел и считывает их 
+		
+	if ( b<0) {		
+			o++;
+			}
+			else if (b>0) {
+			p++;
+			}
+			else if (b==0) {
+			n++;
+			}
+
+	}
+	System.out.println( " количество отрицательных чисел: " + o);
+	System.out.println(" количество положительных чисел: " + p);
+	System.out.println(" количество нулей: " + n);
+	sc.close();
 }
 }
+
