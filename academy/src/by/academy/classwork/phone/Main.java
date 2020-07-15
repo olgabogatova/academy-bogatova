2d6ca4faffb10fabe736514a95b9964fb3fd3f66
olgabogatova <bogatova-1999@mail.ru>
2020-07-15 12:07:39 +0300

    Phone

---
+++ academy/src/by/academy/classwork/phone/Main.java
@@ -0,0 +1,23 @@
+package by.academy.classwork.phone;
+
+public class Main {
+	public static void main(String[] args) {
+		Phone one = new Phone("123456789", 120, "Iphone");
+		Phone two = new Phone("234567891", 234.4, "Xiomi");
+		Phone three = new Phone("345678912", 220,"Samsung");
+		
+		System.out.println("Number: " + one.getNumber()+ ", " + "Weight: " + one.getWeight() + ", "+ "Model: " + one.getModel());
+		System.out.println("Number: " + two.getNumber()+ ", " + "Weight: " + two.getWeight() + ", "+ "Model: " + two.getModel());
+		System.out.println("Number: " + three.getNumber()+ ", " + "Weight: " + three.getWeight() + ", "+ "Model: " + three.getModel());
+		System.out.println(one.receiveCall(null) + "\n" + two.receiveCall(null) + "\n" + three.receiveCall(null));
+		
+	
+		Phone four = new Phone("456789123", 210);
+		System.out.println("Number: " + four.getNumber()+ ", " + "Weight: " + four.getWeight() + ", "+ "Model: " + four.getModel());
+		
+		System.out.println(one.receiveCall(null, null) + "\n" + two.receiveCall(null, null) + "\n" + three.receiveCall(null, null));
+		
+		System.out.println(one.sendMessage("47888", "56666", "233231"));	
+	}
+	
+}
---
+++ academy/src/by/academy/classwork/phone/Phone.java
@@ -0,0 +1,69 @@
+package by.academy.classwork.phone;
+
+import java.util.Arrays;
+
+public class Phone {
+
+	private String number;
+	private double weight;
+	private String model;
+
+	public Phone(String number, double weight, String model) {
+		super();
+		this.number = number;
+		this.weight = weight;
+		this.model = model;
+	}
+
+	public Phone(String number, double weight) {
+		this(number, weight, "Iphone");
+	}
+
+	public Phone(String number, String model) {
+		super();
+		this.number = number;
+		this.model = model;
+	}
+
+	public Phone() {
+		super();
+
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String receiveCall(String name) {
+		return "Calls:" + name;
+	}
+
+	public String receiveCall(String name, String number) {
+		return "Calls:" + name + ", " + "Number:" + number;
+	}
+
+	public String sendMessage(String... numbers) {
+		return Arrays.toString(numbers);
+	}
+
+}
